093d1da5caf0ab9d2aad67dff050b5ba1b010edf
Ilanky <49631121+ilankyn@users.noreply.github.com>
2021-12-08 08:56:41 +0530

    Update lib_stat.jl

--- lib_stat.jl
+++ lib_stat.jl
@@ -5,3 +5,9 @@ mean(x)
 median(skipmissing([10, 32, missing, 41, 75, 96]))
   
 quantile(x, [0.1, 0.5, 0.9])
+
+function sd(x):
+  expectation = mean(x)
+  x = map(i -> (i - expectation)^2, x)
+  std_dev = sqrt(sum(x)/(length(x) - 1))
+  return std_dev
