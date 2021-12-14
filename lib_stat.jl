cdb597dfe54136d4b02316a87959b5fcbeaae88b
Ilanky <49631121+ilankyn@users.noreply.github.com>
2021-12-14 08:31:35 +0530

    Update lib_stat.jl

--- lib_stat.jl
+++ lib_stat.jl
@@ -1,11 +1,15 @@
 using Statistics
+
+#mean
 x = [20:100];
 mean(x)
 
+#median
 median(skipmissing([10, 32, missing, 41, 75, 96]))
-  
+
 quantile(x, [0.1, 0.5, 0.9])
 
+#standard deviation
 function sd(x):
   expectation = mean(x)
   x = map(i -> (i - expectation)^2, x)
